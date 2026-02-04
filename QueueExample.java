c64c7b7e248357d3ce9ebf7ac793f8e31f3945dc
Ajinkya3125 <sajinkya650@gmail.com>
2026-02-04 19:17:52 +0530

    Java file

---
+++ QueueExample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class QueueExample
+{
+    public static void main(String[] args)
+    {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.offer(30);
+        System.out.println(q);
+        System.out.println(q.peek());
+        System.out.println(q.poll());
+        System.out.println(q);
+    }
+}
