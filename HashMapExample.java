11579a9518777f738148a40d31de2cccbac0c449
Ajinkya3125 <sajinkya650@gmail.com>
2026-02-05 20:41:11 +0530

    Update HashMapExample.java

--- HashMapExample.java
+++ HashMapExample.java
@@ -1,16 +1,18 @@
 import java.util.*;
-
-class HashMapExample {
-    public static void main(String[] args) {
+class HashMapExample
+{
+    public static void main(String[] args)
+    {
         HashMap<Integer, String> mp = new HashMap<>();
-        mp.put(1, "Python");
-        mp.put(2, "Java");
-        mp.put(3, "C++");
-        mp.put(4, "C#");
-
+        mp.put(1,"Python");
+        mp.put(2,"Java");
+        mp.put(3,"C++");
+        mp.put(null,"C#");
+        
         System.out.println(mp.get(3));
-
-        for (Map.Entry<Integer, String> e : mp.entrySet()) {
+        
+        for(Map.Entry<Integer, String> e : mp.entrySet())
+        {
             System.out.println(e.getKey() + "=" + e.getValue());
         }
     }
